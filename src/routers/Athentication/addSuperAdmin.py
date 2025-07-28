575ee84f854a6fb94491a56d474d15cce4291a3a
arezoo ghiami <ghiamiarezoo@yahoo.com>
2025-07-28 15:50:50 +0330

    fix registrations

--- src/routers/Athentication/addSuperAdmin.py
+++ src/routers/Athentication/addSuperAdmin.py
@@ -21,20 +21,19 @@ SUPERADMIN_Status = Status.ACTIVE
 def register_superadmin():
     db = SessionLocal()
     try:
-        super_office = db.query(Office).filter(Office.id == 1)
+        # Ensure office with id=1 exists
+        super_office = db.query(Office).filter(Office.id == 1).first()
         if not super_office:
-            super_office = Office(
-                name="all"
-            )
+            super_office = Office(id=1, name="all")
+            db.add(super_office)
+            db.commit()
+
         # Check if the superadmin already exists
         superadmin = db.query(User).filter(User.code == SUPERADMIN_NATIONAL_CODE).first()
         if not superadmin:
             password = '123'
-
-            # Hash the password
             hashed_password = pwd_context.hash(password)
 
-            # Create the superadmin user
             superadmin_user = User(
                 code=SUPERADMIN_NATIONAL_CODE,
                 name=SUPERADMIN_NAME,
@@ -45,9 +44,9 @@ def register_superadmin():
                 office_id=1
             )
 
-            # Add the superadmin to the database
             db.add(superadmin_user)
             db.commit()
+            logging.info("Superadmin user created successfully.")
         else:
             logging.info("Superadmin user already exists.")
     except Exception as e:
@@ -55,3 +54,4 @@ def register_superadmin():
         logging.error(f"Error creating superadmin: {e}")
     finally:
         db.close()
+
