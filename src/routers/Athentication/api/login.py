a384c85a070583cca9f4a1dfd5a575d898de930d
arezoo ghiami <ghiamiarezoo@yahoo.com>
2025-07-30 15:50:12 +0330

    better look

--- src/routers/Athentication/api/login.py
+++ src/routers/Athentication/api/login.py
@@ -18,7 +18,12 @@ templates = Jinja2Templates(directory=TEMPLATES_DIR)
 @router_login.get("/")
 def login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
-
+def normalize_digits(text: str) -> str:
+    translation_table = str.maketrans(
+        "۰۱۲۳۴۵۶۷۸۹" + "٠١٢٣٤٥٦٧٨٩",  # فارسی + عربی
+        "0123456789" * 2
+    )
+    return text.translate(translation_table)
 
 @router_login.post("/login")
 async def login(
@@ -27,8 +32,10 @@ async def login(
         password: str = Form(...),
         db: Session = Depends(get_db),
 ):
-    user = db.query(User).filter(User.code == code).first()
-    if not user or not pwd_context.verify(password, user.password) or user.status == "deactive":
+    normalized_code = normalize_digits(code)
+    normalized_pass = normalize_digits(password)
+    user = db.query(User).filter(User.code == normalized_code).first()
+    if not user or not pwd_context.verify(normalized_pass, user.password) or user.status == "deactive":
         return templates.TemplateResponse(
             "login.html",
             {"request": request, "error": "نام کاربری یا رمز عبور اشتباه است"},
@@ -43,3 +50,10 @@ async def login(
         return RedirectResponse(url="/admin_dashboard", status_code=302)
     else:
         return RedirectResponse(url="/user_dashboard", status_code=302)
+
+
+
+@router_login.get("/logout")
+async def logout(request: Request):
+    request.session.clear()
+    return RedirectResponse(url="/", status_code=302)
